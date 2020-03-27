fb2735f1a5c318c1f556ed46ceb4741436c12c69
winie <61988443+winicosby@users.noreply.github.com>
2020-03-27 09:45:27 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+const name = "Aidelokhai Winifred Aliegbe";
+const courses = ['HTML','CSS','JAVASCRIPT'];
+
+console.log(name);
+console.log(courses);
+
+for(let i = 0; i <= 200; i++){
+  if (i%2 != 0){
+    console.log(i)
+  }
+}
